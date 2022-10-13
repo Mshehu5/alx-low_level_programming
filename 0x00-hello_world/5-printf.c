42b5ad950ccf40644aa9504483ee0bb28d5c56bf
Shehu <Musheu@gmail.com>
2022-10-13 14:23:48 -0700

    programs

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main - this is the begining point of the program
+*void is the function
+*Return: a value of 0
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
