5c1907cb69eaa51270c6d3f10052082751584708
Shehu <Musheu@gmail.com>
2022-10-14 17:29:31 -0700

    Low level programming

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+*main - act as the beginning point of the program that
+*prints all possible combinations of single-digit numbers
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+int r;
+for (r = 48; r < 58; r++)
+{
+putchar(r);
+if (r != 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
