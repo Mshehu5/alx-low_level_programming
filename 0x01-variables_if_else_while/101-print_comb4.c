557dd23f7fc563768dbeff24dece645beb5e392e
Shehu <Musheu@gmail.com>
2022-10-14 22:48:28 -0700

    Low level programming

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -18,16 +18,16 @@ int main(void)
 			for (k = 0; k <= 9; k++)
 			{
 				if (k > j && j > i)
+				{
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(k + '0');
+					if (i != 7 || j != 8 || k != 9)
 					{
-						putchar(i + '0');
-						putchar(j + '0');
-						putchar(k + '0');
-						if (i != 7 || j != 8 || k != 9)
-						{
-							putchar(',');
-							putchar(' ');
-						}
+						putchar(',');
+						putchar(' ');
 					}
+				}
 			}
 		}
 	}
