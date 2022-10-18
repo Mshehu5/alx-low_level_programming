ad69ba0f9364feded808a30b398b5b5e686d925c
Shehu <Musheu@gmail.com>
2022-10-18 14:09:49 -0700

    first task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*print_alphabet - check the code for Holberton School students.
+*
+*Return: xero always.
+*/
+void print_alphabet_x10(void)
+{
+	char alp = 'a';
+
+	while (alp <= 'z')
+	{
+		_putchar(alp);
+		alp++;
+	}
+}
