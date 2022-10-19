ac48f78fafd3a3cdfb1494064da6334d75b7d988
Shehu <Musheu@gmail.com>
2022-10-18 17:02:26 -0700

    task add

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+*print_alphabet- act as the beginning point
+*Return: always return 0
+*/
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int i = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+		for(c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+
+		_putchar('\n');
+	}
+}
