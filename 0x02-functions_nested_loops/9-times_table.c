02214e7f51ce80883cdde015b56c57d0ecda9ecd
Shehu <Musheu@gmail.com>
2022-10-18 19:43:30 -0700

    add task

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,11 +6,10 @@
  */
 void times_table(void)
 {
-  int x, y, z, u, d;
-  
-  for (x = 0; x <= 9; x++)
+	int x, y, z, u, d;
+	for (x = 0; x <= 9; x++)
 	{
-  	for (y = 0; y <= 9; y++)
+		for (y = 0; y <= 9; y++)
 		{
 			z = x * y;
 
