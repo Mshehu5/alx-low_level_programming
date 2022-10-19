c79a92c5404df6ba58a48f3588e00349779b8ab2
Shehu <Musheu@gmail.com>
2022-10-18 18:37:30 -0700

    task add

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,12 +8,12 @@
 */
 int print_sign(int n)
 {
-	if ( n > 0)
+	if (n > 0)
 	{
 		_putchar(43);
 		return (1);
 	}
-	else if ( n == 0 )
+	else if (n == 0)
 	{
 		 _putchar(48);
 		return (0);
@@ -21,6 +21,6 @@ int print_sign(int n)
 	else
 	{
 		 _putchar(45);
-		return(-1);
+		return (-1);
 	}
 }
